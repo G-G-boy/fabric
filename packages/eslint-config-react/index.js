be87b970b78202149bd5d7bf0c591f9d8fc598da
gavin <1159782348@qq.com>
2021-09-28 10:30:41 +0800

    fix: react testing library

--- packages/eslint-config-react/index.js
+++ packages/eslint-config-react/index.js
@@ -2,17 +2,19 @@
  * @type {import("eslint").Linter.Config}
  */
 const config = {
-    extends: [
-        'plugin:react/recommended',
-        '@gavin-fabric/eslint-config-typescript',
-        '"plugin:testing-library/react"',
-    ],
-    plugins: ['react-hooks'],
+    extends: ['plugin:react/recommended', '@gavin-fabric/eslint-config-typescript'],
+    plugins: ['react-hooks', 'testing-library'],
     settings: {
         react: {
             version: '17.0',
         },
     },
+    overrides: [
+        {
+            files: ['**/__tests__/**/*.[jt]s?(x)', '**/?(*.)+(spec|test).[jt]s?(x)'],
+            extends: ['plugin:testing-library/react'],
+        },
+    ],
     rules: {
         // react
         'react/jsx-uses-vars': 'warn',
